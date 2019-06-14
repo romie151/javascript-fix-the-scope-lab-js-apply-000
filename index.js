674a2b59d64f52df1e6cb894e40580311c23db64
voracious-blog-1591 <zelayaromie@gmail.com>
2019-06-14 16:28:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 var animal = 'dog'
 
 function myAnimal() {
-  return animal
+  if (animal === "dog") {
+    animal = "cat"
+    return animal
+  }
+  // return animal
 }
 
 function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  return animal
+  function myAnimal(animal) {
+    if (animal === "dog") {
+      animal = "cat"
+      return animal
+    } else if (animal === "cat") {
+      animal = "dog"
+      return animal
+    }
+    // return animal
+    // }return animal
+  }
 }
 
 function add2(n) {
-  return n + two
-
-  // Feel free to move things around!
   const two = 2
+  return n + two
 }
 
 var funkyFunction = function() {
@@ -26,4 +38,4 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+var theFunk = "FUNKY!"
